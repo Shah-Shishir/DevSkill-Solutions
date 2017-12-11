79df0705cc6d57295499894d77ad4472238fdd49
Shah Newaj Rabbi Shishir <shahshishir.09@gmail.com>
2017-12-12 00:17:08 +0600

    Add files via upload

---
+++ DCP 410 - Sanvi and Magical String.cpp
@@ -0,0 +1,73 @@
+/***
+
+            Bismillahir Rahmanir Rahim
+            Read the name of Allah, who created you!!!
+            Author : Shah Newaj Rabbi Shishir
+            Department of CSE, City University, Bangladesh.
+
+***/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define sf scanf
+#define pf printf
+#define ssf sscanf
+#define spf sprintf
+#define fsf fscanf
+#define fpf fprintf
+#define fast ios_base::sync_with_stdio(0),cin.tie(0),cout.tie(0)
+#define scase sf ("%d",&tc)
+#define sn sf ("%d",&n)
+#define whilecase while (tc--)
+#define eof while (cin >> n)
+#define forloop for (pos=1; pos<=tc; pos++)
+#define arrayloop (i=0; i<n; i++)
+#define cinstr cin >> str
+#define getstr getline (cin,str)
+#define pcase pf ("Case %d: ",pos)
+#define pii pair <int,int>
+#define pb push_back
+#define in insert
+#define llu unsigned long long
+#define lld long long
+#define U unsigned int
+#define endl "\n"
+
+const int MOD = 1000000007;
+const int MAX = 1000005;
+
+int main (void)
+{
+    /*
+    freopen ("input.txt","r",stdin);
+    freopen ("output.txt","w",stdout);
+    */
+
+    int tc,n,i,mx;
+    char str[1001];
+
+    sf ("%d",&tc);
+
+    while (tc--)
+    {
+        sf ("%d\n%s",&n,str);
+
+        map <char,int> mp;
+
+        for (i=0; i<n; i++)
+            mp[str[i]]++;
+
+        mx = 0;
+
+        map <char,int> :: iterator it;
+
+        for (it=mp.begin(); it!=mp.end(); it++)
+            if ((it->second) > mx)
+                mx = it->second;
+
+        pf ("%d\n",n-mx);
+    }
+
+    return 0;
+}
