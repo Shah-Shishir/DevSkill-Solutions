bb0a901a690f6689a4a32baabac59308e8db6f2d
Shah Newaj Rabbi Shishir <shahshishir.09@gmail.com>
2018-04-28 04:41:07 +0600

    Add files via upload

---
+++ DCP 517 - FIFA 2030.cpp
@@ -0,0 +1,80 @@
+/***
+
+            Bismillahir Rahmanir Rahim
+            Read the name of Allah, who created you!!!
+            Author : Shah Newaj Rabbi Shishir
+            Department of CSE, City University, Bangladesh.
+
+***/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define sf scanf
+#define pf printf
+#define ssf sscanf
+#define spf sprintf
+#define fsf fscanf
+#define fpf fprintf
+#define fast ios_base::sync_with_stdio(0),cin.tie(0),cout.tie(0)
+#define scase sf ("%d",&tc)
+#define sn sf ("%d",&n)
+#define whilecase while (tc--)
+#define eof while (cin >> n)
+#define forloop for (pos=1; pos<=tc; pos++)
+#define arrayloop (i=0; i<n; i++)
+#define cinstr cin >> str
+#define getstr getline (cin,str)
+#define pcase pf ("Case %d: ",pos)
+#define vi vector <int>
+#define pii pair <int,int>
+#define mii map <int,int>
+#define pb push_back
+#define in insert
+#define llu unsigned long long
+#define lld long long
+#define U unsigned int
+#define endl "\n"
+
+const int MOD = 1000000007;
+const int MAX = 1000005;
+
+int SetBit (int n, int x) { return n | (1 << x); }
+int ClearBit (int n, int x) { return n & ~(1 << x); }
+int ToggleBit (int n, int x) { return n ^ (1 << x); }
+int CheckBit (int n, int x) { return (bool)(n & (1 << x)); }
+
+int main (void)
+{
+    /*
+    freopen ("input.txt","r",stdin);
+    freopen ("output.txt","w",stdout);
+    */
+
+    int tc,pos,n,i,g,maxi;
+    char str[22],ans[22];
+
+    sf ("%d",&tc);
+
+    for (pos=1; pos<=tc; pos++)
+    {
+        sf ("%d",&n);
+
+        maxi = -1;
+
+        for (i=1; i<=n; i++)
+        {
+            sf ("%s %d",str,&g);
+
+            if (g > maxi)
+            {
+                maxi = g;
+                strcpy (ans,str);
+            }
+        }
+
+        pf ("Winner: \\%s\/\n",ans);
+    }
+
+    return 0;
+}
