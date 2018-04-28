8b847b4adfe143ffa738da55f07966a23dd93c53
Shah Newaj Rabbi Shishir <shahshishir.09@gmail.com>
2018-04-28 18:10:32 +0600

    Add files via upload

---
+++ DCP 382 - Prime and Even-Odd.cpp
@@ -0,0 +1,54 @@
+/***
+
+            Bismillahir Rahmanir Rahim
+            Read the name of Allah, who created you!!!
+            Author : Shah Newaj Rabbi Shishir
+            Department of CSE, City University, Bangladesh.
+
+***/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define sf scanf
+#define pf printf
+#define fast ios_base::sync_with_stdio(0),cin.tie(0),cout.tie(0)
+#define scase sf ("%d",&tc)
+#define sn sf ("%d",&n)
+#define whilecase while (tc--)
+#define eof while (cin >> n)
+#define forloop for (pos=1; pos<=tc; pos++)
+#define arrayloop (i=0; i<n; i++)
+#define cinstr cin >> str
+#define getstr getline (cin,str)
+#define pcase pf ("Case %d: ",pos)
+#define pii pair <int,int>
+#define pb push_back
+#define in insert
+#define llu unsigned long long
+#define lld long long
+#define U unsigned int
+#define endl "\n"
+
+const int MOD = 1000000007;
+const int MAX = 1000005;
+
+int main (void)
+{
+    /*
+    freopen ("input.txt","r",stdin);
+    freopen ("output.txt","w",stdout);
+    */
+
+    llu n,m;
+
+    while (sf ("%llu %llu",&n,&m) != EOF)
+    {
+        if ((n == m && n == 1) || (n > 1 && m > 1))
+            pf ("Even\n");
+        else
+            pf ("Odd\n");
+    }
+
+    return 0;
+}
